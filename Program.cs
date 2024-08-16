343c3291bacd2a637927123fd04a872b3549c48c
Barry Anderson <barryanderson60@outlook.com>
2024-08-16 12:27:00 +0100

    Use a SQL Server database

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using GuitarMan.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace GuitarMan
 {
     public class Program
@@ -8,6 +11,8 @@ namespace GuitarMan
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
