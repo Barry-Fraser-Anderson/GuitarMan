288838812d10de8fa87a1175b5f706b99566fc97
Barry Anderson <barryanderson60@outlook.com>
2024-08-16 13:34:09 +0100

    Seed the Songs table

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -10,5 +10,15 @@ namespace GuitarMan.Data
         }
 
         public DbSet<Song> Songs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Song>().HasData(
+                new Song { Id = 1, Artist = "4 Non Blondes", Title = "What's Up?", Link = "https://www.youtube.com/watch?v=-7wsYYY1o98" },
+                new Song { Id = 2, Artist = "Johnny Cash", Title = "Hurt", Link = "https://www.youtube.com/watch?v=CrQguKCnYTI" },
+                new Song { Id = 3, Artist = "Neil Young", Title = "Heart of Gold", Link = "https://www.youtube.com/watch?v=U0MKyH48OhU" }
+            );
+        }
+
     }
 }
---
+++ Migrations/20240816123148_SeedSongsTable.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace GuitarMan.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedSongsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Songs",
+                columns: new[] { "Id", "Artist", "Link", "Title" },
+                values: new object[,]
+                {
+                    { 1, "4 Non Blondes", "https://www.youtube.com/watch?v=-7wsYYY1o98", "What's Up?" },
+                    { 2, "Johnny Cash", "https://www.youtube.com/watch?v=CrQguKCnYTI", "Hurt" },
+                    { 3, "Neil Young", "https://www.youtube.com/watch?v=U0MKyH48OhU", "Heart of Gold" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Songs",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Songs",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Songs",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
