8ff9fecf18392141b6df4329bd346d5d2527717e
Barry Anderson <barryanderson60@outlook.com>
2024-08-19 17:11:21 +0100

    Add song Description field

---
+++ Migrations/20240819155340_AddDescriptionToSongsTable.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GuitarMan.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDescriptionToSongsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Songs",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Songs",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Description",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Songs",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Description",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Songs",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Description",
+                value: null);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Songs");
+        }
+    }
+}
--- Models/Song.cs
+++ Models/Song.cs
@@ -9,6 +9,8 @@ namespace GuitarMan.Models
         public string Artist { get; set; } = String.Empty;
         [Required]
         public string Title { get; set; } = String.Empty;
+        
+        public string? Description { get; set; }
         [Required]
         public string Link { get; set; } = String.Empty;
     }
