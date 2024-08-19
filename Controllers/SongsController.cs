007fb668620100388e7da38e668d03ca0fd197ca
Barry Anderson <barryanderson60@outlook.com>
2024-08-19 15:02:05 +0100

    Implement Edit song

--- Controllers/SongsController.cs
+++ Controllers/SongsController.cs
@@ -19,23 +19,40 @@ namespace GuitarMan.Controllers
             return View(songs);
         }
 
-        public IActionResult CreateEditForm()
+        public IActionResult Create()
         {
             return View();
         }
-         
+
+     
+        [HttpPost]
+        public IActionResult Create(Song song)
+        {
+            _dbContext.Songs.Add(song);
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
         public IActionResult Delete(int id)
         {
-            var song = _dbContext.Songs.SingleOrDefault(song => song.Id == id);
+            var song = _dbContext.Songs.FirstOrDefault(song => song.Id == id);
             _dbContext.Songs.Remove(song);
             _dbContext.SaveChanges();
 
             return RedirectToAction("Index");
         }
 
-        public IActionResult CreateEditOk(Song song)
+        public IActionResult Edit(int id)
         {
-            _dbContext.Songs.Add(song);
+            var song = _dbContext.Songs.FirstOrDefault(song => song.Id == id);
+            return View(song);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Song song)
+        {
+            _dbContext.Songs.Update(song);
             _dbContext.SaveChanges();
 
             return RedirectToAction("Index");
