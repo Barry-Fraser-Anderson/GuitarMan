3788612ad1680379f7490e39583c97b060886825
Barry Anderson <barryanderson60@outlook.com>
2024-08-20 12:49:56 +0100

    Remove Privacy page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,12 +17,7 @@ namespace GuitarMan.Controllers
         {
             return View();
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
+ 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
